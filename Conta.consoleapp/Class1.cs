02f9d19397282e77967d6220f636d9c8aa395a87
Feldhaus <arturfeldhaus12@gmail.com>
2022-03-10 17:11:44 -0300

    Add project files.

---
+++ Conta.consoleapp/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta.consoleapp
+{
+    public class conta
+    {
+        public double saldo;
+        public string status;
+        public double limite;
+        public int hisorico;
+        public int id;
+        public double saldo2;
+        public double vsaque;
+        public double vdeposito;
+        public double vtransferencia;
+        public double nsaldo;
+        
+       
+        
+        public void extrato(double vsauqe, double vdeposito, double vtransferencia)
+        {
+            Console.WriteLine("Voce depositou: {0}", vdeposito);
+            Console.WriteLine("Voce sacou: {0}", vsauqe);
+            Console.WriteLine("Voce transferiu: {0}", vtransferencia);
+        }
+       public void transferencia(int contaquerecebe, double vtransfrencia)
+       {
+          if(contaquerecebe == 1)
+          {
+                saldo = vtransfrencia + saldo;
+                saldo2 = saldo2 - vtransferencia;
+          }
+          else if (contaquerecebe == 2)
+          {
+                saldo2 = vtransfrencia + saldo;
+                saldo = saldo - vtransferencia;
+          }   
+        }
+        public void versaldo(double vsauqe, double vdeposito, double vtransferencia)
+        {
+            saldo = saldo - vsauqe + vdeposito - vtransferencia;
+
+        }
+        public void infoc1()
+        {
+            Console.WriteLine("O saldo da conta é: {0}", saldo);
+            Console.Write("A conta ");
+            Console.WriteLine(status);
+            Console.WriteLine("O limite da conta é: {0}", limite);
+            Console.WriteLine("O id da conta é: {0}", id);
+        }
+        public void infoc2()
+        {
+            Console.WriteLine("O saldo da conta é: {0}", saldo2);
+            Console.Write("A conta ");
+            Console.WriteLine(status);
+            Console.WriteLine("O limite da conta é: {0}", limite);
+            Console.WriteLine("O id da conta é: {0}", id);
+        }
+
+
+    }
+    
+}
---
+++ Conta.consoleapp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Conta.consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            conta conta1 = new conta();           
+            conta1.saldo = 1000;
+            conta1.status = "é Especial";
+            conta1.id = 1;
+            conta1.limite = 2000;
+            
+            conta conta2 = new conta();
+            conta2.saldo2 = 500;
+            conta2.status = "Não Especial";
+            conta2.id = 2 ;
+            conta2.limite = 1000;
+
+            Console.WriteLine("INFORMAÇÕES CONTA 1!!");
+            Console.WriteLine("==============================");           
+            conta1.transferencia(1, 500);
+            conta1.versaldo(200, 200, 200);
+            conta1.extrato(200, 200, 200);
+            Console.WriteLine("==============================");
+            conta1.infoc1();
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("INFORMAÇÕES CONTA 2!!");
+            Console.WriteLine("==============================");
+            conta2.transferencia(2, 700);
+            conta2.versaldo(200, 300, 280);
+            conta2.extrato(200, 300, 280);
+            Console.WriteLine("==============================");
+            conta2.infoc2();
+            Console.WriteLine("==============================");
+
+            Console.ReadLine();
+        }
+    }
+}
